01de4466527865471e253f2cbd92e1e179abc083
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-22 23:34:24 +0100

    My second commit

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - prints a string based on printf function
+ * @format: string to print
+ * @...: variadic parameters
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	//expresssions;
+	//return (0); 
+}
